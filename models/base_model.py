8906b847fce7ca477e0558a7eb570f7b7bc832fa
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2023-08-08 19:53:08 +0000

    file upload

---
+++ models/base_model.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+from uuid import uuid4
+from datetime import datetime
+""" This module contains a superclass BaseModel that will be inherited"""
+class BaseModel:
+    """A super class that subsquent classes will inherit from """
+    def __init__(self):
+        """the object constructor, it initialises instances of BaseModel"""
+        self.id = str(uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+    def __str__(self):
+        """returns the string representation of the class BaseModel"""
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+    def save(self):
+        """updates the public instance attribute updated_at 
+            with the current datetime"""
+        self.updated_at = datetime.now()
+    def to_dict(self):
+        """returns a dictionary containing all keys/values 
+            of __dict__ of the instance"""
+        dic = self.__dict__
+        dic["__class__"] = self.__class__.__name__
+        dic["created_at"] = self.created_at.isoformat()
+        dic["updated_at"] = self.updated_at.isoformat()
+        return dic
