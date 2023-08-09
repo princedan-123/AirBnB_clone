17a1b7748d0ff20ad08aa4792b13e612ceca5626
Daniel Ebuka Mabia <danielebukamabia@gmail.com>
2023-08-09 12:17:46 +0000

    made it pep8 compliant

--- models/base_model.py
+++ models/base_model.py
@@ -2,30 +2,36 @@
 from uuid import uuid4
 from datetime import datetime
 """ This module contains a superclass BaseModel that will be inherited"""
+
+
 class BaseModel:
     """A super class that subsquent classes will inherit from """
     def __init__(self, *args, **kwargs):
         """the object constructor, it initialises instances of BaseModel"""
-        if kwargs and len(args) == 0:
+        if kwargs and not args:
             for key, value in kwargs.items():
                 if key == "created_at" or key == "updated_at":
                     setattr(self, key, datetime.fromisoformat(value))
-                elif key != "__class__" :
+                elif key != "__class__":
                     setattr(self, key, value)
         else:
             self.id = str(uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+
     def __str__(self):
         """returns the string representation of the class BaseModel"""
         return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+
     def save(self):
-        """updates the public instance attribute updated_at 
+        """updates the public instance attribute updated_at
             with the current datetime"""
         self.updated_at = datetime.now()
+
     def to_dict(self):
-        """returns a dictionary containing all keys/values 
-            of __dict__ of the instance"""
+        """returns a dictionary containing all keys/values
+            of __dict__ of the instance
+        """
         dic = self.__dict__
         dic["__class__"] = self.__class__.__name__
         dic["created_at"] = self.created_at.isoformat()
